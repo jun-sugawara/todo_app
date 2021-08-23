498cee2f599667d0eff476c9960faee72fbf622a
jun-sugawara <ex.24karats.0110@i.softbank.jp>
2021-08-23 11:59:37 +0900

    styledコンポーネントの中身を記述

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -10,8 +10,37 @@ const Nabvar = styled.nav`
   background: #dbfffe;
   min-height: 8vh;
   display: flex;
-  justify-content: center;
+  justify-content: space-around;
   align-items: center;
+`
+
+const Logo = styled.div`
+  font-weight: bold;
+  font-size: 23px;
+  letter-spacing: 3px;
+`
+
+const NavItems = styled.ul`
+  display: flex;
+  width: 400px;
+  max-width: 40%;
+  justify-content: space-around;
+  list-style: none;
+`
+
+const NavItem = styled.li`
+  font-size: 19px;
+  font-weight: bold;
+  opacity: 0.7;
+  &:hover {
+    opacity: 1;
+  }
+`
+
+const Wrapper = styled.div`
+  width: 700px;
+  max-width: 85%;
+  margin: 20px auto;
 `
 
 function App() {
